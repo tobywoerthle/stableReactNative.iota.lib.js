73ce8b631808fc2d28d2bdf53efbf642b01688b3
Toby Woerthle <tobywoerthle@users.noreply.github.com>
2018-01-02 22:33:20 -0500

    Initial Commit
    Base app

---
+++ App.js
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import { multisig, transfer } from "./node_modules/iota.flash.js";
+
+import IOTA from 'iota.lib.js';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
+  render() {
+	  
+	// Create IOTA instance directly with provider
+	var iota = new IOTA({
+		'provider': 'http://localhost:14265'
+	});  
+	  
+    return (
+       <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} >
+		<Text style={styles.welcome}>
+          Welcome to React Native!{"\n"}
+		  iota.lib.js version: {iota.version}
+        </Text>
+		</View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
